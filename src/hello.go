59ee9e938c6d82680b66bc7db6563e448547aeec
JEONGCHANHEE <jeongchanhee@JEONGCHANHEEui-MacBookPro.local>
2022-03-18 18:42:42 +0900

    code test

--- src/hello.go
+++ src/hello.go
@@ -4,10 +4,10 @@ import "fmt" //문자열 출력기능
 
 func main() { 
 	var a int = 10
-	vas msg string = "Hello Variable"
+	var msg string = "Hello Variable"
 
 	a = 20
 	msg = "Good Moringing!"
-	fmt.PrintIn(msg, a)
+	fmt.Println(msg, a)
 	fmt.Printf("hello, world\n") 
 }
