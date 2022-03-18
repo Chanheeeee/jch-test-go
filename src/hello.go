a0c736c7fd1a4ba44b517cccb17e5932117020e7
JEONGCHANHEE <jeongchanhee@JEONGCHANHEEui-MacBookPro.local>
2022-03-18 18:40:46 +0900

    test

--- src/hello.go
+++ src/hello.go
@@ -1,5 +1,13 @@
 package main 
-import "fmt" 
+
+import "fmt" //문자열 출력기능
+
 func main() { 
+	var a int = 10
+	vas msg string = "Hello Variable"
+
+	a = 20
+	msg = "Good Moringing!"
+	fmt.PrintIn(msg, a)
 	fmt.Printf("hello, world\n") 
 }
